60801b7dfa9b1667ad68bc261620e8146414a215
Andrew SASSOYE <andrew@sassoye.be>
2020-11-27 14:15:15 +0100

    interro1: Q5

---
+++ i1/src/statsample.c
@@ -0,0 +1,3 @@
+
+
+#include "statsample.h"
---
+++ i1/src/statsample.h
@@ -0,0 +1,12 @@
+
+
+#ifndef DEV3_STATSAMPLE_H
+#define DEV3_STATSAMPLE_H
+
+typedef struct StatSample {
+    int count;
+    int sum;
+    int *data;
+} StatSample;
+
+#endif //DEV3_STATSAMPLE_H
