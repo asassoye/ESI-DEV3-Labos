fd8a19f3fc323bdb218a3b314343a435ba5e9a58
Andrew SASSOYE <andrew@sassoye.be>
2020-11-27 14:46:14 +0100

    interro1: Q8

--- i1/src/statsample.c
+++ i1/src/statsample.c
@@ -2,11 +2,14 @@
 #include <stdlib.h>
 #include "statsample.h"
 
+static void double_capacity(StatSample *s);
+
 void init_stat(StatSample *s) {
     assert(s != NULL);
     s->count = 0;
     s->sum = 0;
     s->data = malloc(sizeof(int) * 10);
+    s->capacity = 10;
 }
 
 void free_stat(StatSample *s) {
@@ -15,4 +18,27 @@ void free_stat(StatSample *s) {
 
     free(s->data);
     s->data = NULL;
+    s->capacity = 0;
+}
+
+void update_stat(int n, StatSample *s) {
+    assert(s != NULL);
+    assert(s->data != NULL);
+
+    if (s->capacity == s->count) {
+        double_capacity(s);
+    }
+
+    s->data[s->count] = n;
+    (s->count)++;
+    s->sum += n;
+}
+
+static void double_capacity(StatSample *s) {
+    assert(s != NULL);
+    assert(s->data != NULL);
+
+    int newCapacity = s->capacity * 2;
+    s->data = realloc(s->data, sizeof(int) * newCapacity);
+    s->capacity = newCapacity;
 }
--- i1/src/statsample.h
+++ i1/src/statsample.h
@@ -7,10 +7,13 @@ typedef struct StatSample {
     int count;
     int sum;
     int *data;
+    int capacity;
 } StatSample;
 
 void init_stat(StatSample *s);
 
 void free_stat(StatSample *s);
+
+void update_stat(int n, StatSample *s);
 
 #endif //DEV3_STATSAMPLE_H
